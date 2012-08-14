2de7d4f289235ff994118227ee4803eb24c541b4
daisy1754 <d-part@hotmail.co.jp>
2012-08-14 19:04:26 +0900

    ask for login when user have used chawan but not login at the time

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 var HatenaClient = require('./hatena_client.js'),
     SettingJSON = localStorage.getItem('chawan');
 
+function askLogin(escapedLocation, name, password) {
+  window.location.href = 'https://www.hatena.ne.jp/login?'
+  	  + 'location=' + escapedLocation + (name ? ('&name=' + name) : '')
+  	  + (password ? ('&password=' + password) : '');
+}
+
 var Setting = JSON.parse(SettingJSON || '{}');
 if (window.location.search === '?config') {
   document.addEventListener("DOMContentLoaded", function () {
-    var preTag = document.body.children.item(0),
-        myNameJSON = JSON.parse(preTag.innerHTML);
+	var preTag = document.body.children.item(0), 
+	    myNameJSON = JSON.parse(preTag.innerHTML);
     if (myNameJSON.login) {
       var View = require('./view.js'), Config = require('./config.js');
       var configView = new View.Config({
@@ -13,15 +19,21 @@ if (window.location.search === '?config') {
         el: document.body
       });
     } else {
-      // ask for login
-      window.location.href = 'https://www.hatena.ne.jp/login?'
-    	  + 'location=http%3A//b.hatena.ne.jp/my.name%3Fconfig';
+      askLogin('http%3A//b.hatena.ne.jp/my.name%3Fconfig');
     }
   }, false);
 } else {
   if (window.location.pathname === '/my.name') {
     if (Setting.name && Setting.rks) {
-      var client = new HatenaClient(Setting.name, Setting.rks)
+      document.addEventListener("DOMContentLoaded", function () {
+    	var preTag = document.body.children.item(0), 
+    	    myNameJSON = JSON.parse(preTag.innerHTML);
+        if (!myNameJSON.login) {
+    	  askLogin('http%3A//b.hatena.ne.jp/my.name%3Fconfig',
+    			  Setting.name, Setting.password);
+    	}
+      });
+      var client = new HatenaClient(Setting.name, Setting.rks);
     } else {
       window.location.href = '?config';
     }
