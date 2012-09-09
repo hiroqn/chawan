b3a246a8ca148be9b66c0f1e198c54cdd7a07307
hiroqn <hiroqn1008@gmail.com>
2012-09-09 20:38:45 +0900

    bug fix delete template bookmark

--- src/template.js
+++ src/template.js
@@ -3,7 +3,6 @@ var file = require('./file.js'),
 module.exports = {
   folder: _.template(file.folder),
   dialog: _.template(file.dialog),
-  bookmarks: _.template(file.bookmark),
   nav: _.template(file.nav),
   config: _.template(file.config),
   editor: _.template(file.editor)
